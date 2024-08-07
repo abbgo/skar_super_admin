e8b92e611ec4bdc39950882d343e59d5dd520248
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-07 13:01:06 +0500

    fix: update LoginImageAndText part widget

--- lib/pages/login/parts/login_image_and_text.dart
+++ lib/pages/login/parts/login_image_and_text.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:skar_super_admin/helpers/functions/screen.dart';
 
 class LoginImageAndText extends StatelessWidget {
   const LoginImageAndText({super.key});
@@ -13,7 +14,11 @@ class LoginImageAndText extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset("assets/images/logo.jpg", fit: BoxFit.cover, height: 400),
+          Image.asset(
+            "assets/images/logo.jpg",
+            fit: BoxFit.cover,
+            width: screenProperties(context).width * .3,
+          ),
           Text(
             '${lang.welcome} !',
             style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 48),
