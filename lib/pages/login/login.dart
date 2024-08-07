05b5e4ee2c05ce3e6537d8e38b5cc797731466de
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-07 12:34:10 +0500

    feat: add LoginImageAndText part widget

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:skar_super_admin/pages/login/parts/login_image_and_text.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Login Page')),
+    return const Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            LoginImageAndText(),
+            Text('Inputlar'),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/pages/login/parts/login_image_and_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginImageAndText extends StatelessWidget {
+  const LoginImageAndText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Surat we soz');
+  }
+}
