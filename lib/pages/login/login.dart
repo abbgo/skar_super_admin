d3df186d446de549df46812ae7fe8ea70a28e4e3
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-07 12:52:37 +0500

    feat: add logoColor color variable and update LoginPage

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:skar_super_admin/pages/login/parts/login_image_and_text.dart';
 import 'package:skar_super_admin/pages/login/parts/login_inputs.dart';
+import 'package:skar_super_admin/styles/colors.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      backgroundColor: logoColor,
+      body: const Center(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 Color elevatedButtonColor = const Color(0xff1e4a71);
 Color scaffoldColorDarkTheme = const Color(0xff212121);
 Color dialogColorDarkTheme = const Color(0xff424242);
+Color logoColor = const Color(0xfff08421);
