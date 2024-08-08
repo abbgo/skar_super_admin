e093850a31ce62d78ccac3604a0d24d56b85e15b
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-08 10:04:20 +0500

    feat: add passwordVisibleProvider provider

---
+++ lib/providers/pages/login.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_super_admin/notifiers/pages/login.dart';
+
+var passwordVisibleProvider =
+    StateNotifierProvider.autoDispose<PasswordVisibleNotifier, bool>(
+  (ref) => PasswordVisibleNotifier(),
+);
