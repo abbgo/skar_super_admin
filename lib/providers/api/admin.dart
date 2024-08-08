3d67343779355af134483544bbaaaffb2ea4e2ab
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-08 10:47:39 +0500

    fix: update loginAdminProvider provider

--- lib/providers/api/admin.dart
+++ lib/providers/api/admin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_super_admin/models/admin.dart';
 import 'package:skar_super_admin/services/api/admin.dart';
 
 final adminApiServiceProvider =
