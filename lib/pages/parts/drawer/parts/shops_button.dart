165ce9053ea515f14b47811286fde76431470201
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-08 12:39:47 +0500

    feat: add ShopsPage

--- lib/pages/parts/drawer/parts/shops_button.dart
+++ lib/pages/parts/drawer/parts/shops_button.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:skar_super_admin/helpers/methods/navigators.dart';
 import 'package:skar_super_admin/styles/colors.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ShopsButton extends StatelessWidget {
   const ShopsButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var lang = AppLocalizations.of(context)!;
+
     return ListTile(
       leading: Icon(Icons.storefront, color: logoColor),
       title: Text(
-        'Dukanlar',
+        lang.shops,
         style: TextStyle(
           color: elevatedButtonColor,
           fontWeight: FontWeight.bold,
---
+++ lib/pages/shops/shops.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShopsPage extends StatelessWidget {
+  const ShopsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Shops page'),
+      ),
+    );
+  }
+}
