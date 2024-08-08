17c4c672b2aa417981a149528956bc6b6032fd10
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-08 11:38:41 +0500

    feat: add DrawerPart part widget

--- lib/pages/dashboard/dashboard.dart
+++ lib/pages/dashboard/dashboard.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:skar_super_admin/pages/parts/drawer/drawer.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Dashboard'),
-      ),
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(child: Text('Dashboard')),
+      drawer: const DrawerPart(),
     );
   }
 }
