e8ad6a68b8c3c79657b9d5d0cf30ca1fc1b39702
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-09 11:31:03 +0500

    fix: update ShopsButton part widget

--- lib/pages/parts/drawer/parts/shops_button.dart
+++ lib/pages/parts/drawer/parts/shops_button.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_super_admin/helpers/methods/navigators.dart';
 import 'package:skar_super_admin/pages/shops/shops.dart';
+import 'package:skar_super_admin/providers/pages/drawer.dart';
 import 'package:skar_super_admin/styles/colors.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class ShopsButton extends StatelessWidget {
+class ShopsButton extends ConsumerWidget {
   const ShopsButton({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     var lang = AppLocalizations.of(context)!;
+    int selectedDrawerButton = ref.watch(selectedDrawerButtonProvider);
 
     return ListTile(
       leading: Icon(Icons.storefront, color: logoColor),
+      tileColor: selectedDrawerButton == 1 ? elevatedButtonColor : null,
       title: Text(
         lang.shops,
         style: TextStyle(
-          color: elevatedButtonColor,
+          color: selectedDrawerButton == 1 ? Colors.white : elevatedButtonColor,
           fontWeight: FontWeight.bold,
         ),
       ),
-      onTap: () => goToPage(context, const ShopsPage(), true),
+      onTap: () {
+        ref.read(selectedDrawerButtonProvider.notifier).state = 1;
+        goToPage(context, const ShopsPage(), true);
+      },
     );
   }
 }
