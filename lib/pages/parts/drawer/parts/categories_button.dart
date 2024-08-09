82141c15762d317f323ec4071d8213d7227e5dda
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-09 11:33:00 +0500

    fix: update CategoriesButton part widget

--- lib/pages/parts/drawer/parts/categories_button.dart
+++ lib/pages/parts/drawer/parts/categories_button.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_super_admin/helpers/methods/navigators.dart';
 import 'package:skar_super_admin/pages/categories/categories.dart';
+import 'package:skar_super_admin/providers/pages/drawer.dart';
 import 'package:skar_super_admin/styles/colors.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class CategoriesButton extends StatelessWidget {
+class CategoriesButton extends ConsumerWidget {
   const CategoriesButton({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     var lang = AppLocalizations.of(context)!;
+    int selectedDrawerButton = ref.watch(selectedDrawerButtonProvider);
 
     return ListTile(
       leading: Icon(Icons.view_cozy_outlined, color: logoColor),
+      tileColor: selectedDrawerButton == 2 ? elevatedButtonColor : null,
       title: Text(
         lang.categories,
         style: TextStyle(
-          color: elevatedButtonColor,
+          color: selectedDrawerButton == 2 ? Colors.white : elevatedButtonColor,
           fontWeight: FontWeight.bold,
         ),
       ),
-      onTap: () => goToPage(context, const CategoriesPage(), true),
+      onTap: () {
+        ref.read(selectedDrawerButtonProvider.notifier).state = 2;
+        goToPage(context, const CategoriesPage(), true);
+      },
     );
   }
 }
--- lib/providers/pages/drawer.dart
+++ lib/providers/pages/drawer.dart
@@ -1,3 +1,3 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-var selectedDrawerButtonProvider = StateProvider.autoDispose<int>((ref) => 0);
+var selectedDrawerButtonProvider = StateProvider<int>((ref) => 0);
