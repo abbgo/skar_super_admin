1d0d77657bf37ea775af00a56f29d6f541eec6c6
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-09 11:38:30 +0500

    feat: add WaitingShops and SuccessShops part widgets and update ShopsPage

--- lib/pages/shops/shops.dart
+++ lib/pages/shops/shops.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:skar_super_admin/pages/parts/drawer/drawer.dart';
+import 'package:skar_super_admin/pages/shops/parts/success_shops.dart';
+import 'package:skar_super_admin/pages/shops/parts/waiting_shops.dart';
 
-class ShopsPage extends StatelessWidget {
+class ShopsPage extends StatefulWidget {
   const ShopsPage({super.key});
+
+  @override
+  State<ShopsPage> createState() => _ShopsPageState();
+}
+
+class _ShopsPageState extends State<ShopsPage> with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorWeight: 2,
+          tabs: const [
+            Text('Garasylyan dukanlar'),
+            Text('Dukanlar'),
+          ],
+        ),
+      ),
       drawer: const DrawerPart(),
-      body: const Center(child: Text('Shops page')),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: TabBarView(
+          controller: _tabController,
+          children: const [WaitingShops(), SuccessShops()],
+        ),
+      ),
     );
   }
 }
