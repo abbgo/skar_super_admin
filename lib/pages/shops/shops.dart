5e8b67e74a03325fcbc9b5ecba8a5519f7ed897d
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-09 11:48:27 +0500

    fix: update ShopsPage

--- lib/pages/shops/shops.dart
+++ lib/pages/shops/shops.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:skar_super_admin/pages/parts/drawer/drawer.dart';
 import 'package:skar_super_admin/pages/shops/parts/success_shops.dart';
 import 'package:skar_super_admin/pages/shops/parts/waiting_shops.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ShopsPage extends StatefulWidget {
   const ShopsPage({super.key});
@@ -27,14 +28,17 @@ class _ShopsPageState extends State<ShopsPage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    var lang = AppLocalizations.of(context)!;
+
     return Scaffold(
       appBar: AppBar(
         bottom: TabBar(
           controller: _tabController,
           indicatorWeight: 2,
-          tabs: const [
-            Text('Garasylyan dukanlar'),
-            Text('Dukanlar'),
+          labelStyle: const TextStyle(fontWeight: FontWeight.bold),
+          tabs: [
+            Text(lang.shopsToCheck),
+            Text(lang.activeShops),
           ],
         ),
       ),
