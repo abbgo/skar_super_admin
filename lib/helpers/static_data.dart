e92dba0a4e56e2f6d0ddedeaf7cfd216db07eac2
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-10 07:13:37 +0500

    feat: add tokenHeader method

--- lib/helpers/static_data.dart
+++ lib/helpers/static_data.dart
@@ -2,3 +2,10 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 String apiUrl = dotenv.env['API_URL']!;
 String pathUrl = dotenv.env['PATH_URL']!;
+
+Map<String, String>? tokenHeader(String accessToken) {
+  return {
+    'Authorization': 'Bearer $accessToken',
+    'Content-Type': 'application/json'
+  };
+}
