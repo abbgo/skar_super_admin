25f9e1154a1712709ae7c2703e346e528c8368ca
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-10 08:00:59 +0500

    feat: add SearchInput part widget

---
+++ lib/pages/parts/search_input.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:skar_super_admin/helpers/methods/input.dart';
+import 'package:skar_super_admin/styles/colors.dart';
+
+class SearchInput extends StatelessWidget {
+  const SearchInput({super.key, this.onSubmitted, required this.label});
+
+  final void Function(String)? onSubmitted;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      child: TextField(
+        textInputAction: TextInputAction.search,
+        textAlignVertical: TextAlignVertical.center,
+        cursorColor: elevatedButtonColor,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.only(bottom: 5, left: 20),
+          focusedBorder: inputBorder(),
+          border: inputBorder(),
+          labelText: ' $label ',
+        ),
+        onSubmitted: onSubmitted,
+      ),
+    );
+  }
+}
