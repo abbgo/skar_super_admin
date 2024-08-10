2f0f6a578dde6b1da8c8c1e015ab87da9bcfbab8
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-10 07:59:58 +0500

    fix: update SearchWaintingShops part widget

--- lib/pages/waiting_shops/parts/search_wainting_shops.dart
+++ lib/pages/waiting_shops/parts/search_wainting_shops.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SearchWaintingShops extends StatelessWidget {
   const SearchWaintingShops({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Gozleg');
+    var lang = AppLocalizations.of(context)!;
+
+    return Text(lang.searchShop);
   }
 }
