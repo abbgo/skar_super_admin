ae4a38cbd0d71eda2b8957c7a613794a1d643bc4
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-10 07:56:32 +0500

    feat: add SearchWaintingShops part widget and update myAppBar method

--- lib/pages/parts/my_app_bar/my_app_bar.dart
+++ lib/pages/parts/my_app_bar/my_app_bar.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:skar_super_admin/pages/parts/my_app_bar/parts/avatar.dart';
 import 'package:skar_super_admin/pages/parts/my_app_bar/parts/log_out_button.dart';
+import 'package:skar_super_admin/pages/waiting_shops/parts/search_wainting_shops.dart';
 
-AppBar myAppBar(String page) => AppBar(
-      centerTitle: false,
-      title: Text('Gozleg'),
-      actions: const [
-        AvatarPart(),
-        LogOutButton(),
-      ],
-    );
+AppBar myAppBar(String page) {
+  Widget titlePage = const SizedBox();
+
+  switch (page) {
+    case 'waiting_shops':
+      titlePage = const SearchWaintingShops();
+      break;
+    default:
+  }
+
+  return AppBar(
+    centerTitle: false,
+    title: titlePage,
+    actions: const [
+      AvatarPart(),
+      LogOutButton(),
+    ],
+  );
+}
