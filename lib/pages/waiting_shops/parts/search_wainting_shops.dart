4b3b0b789244a4f851007acf092d0d79b8e6c567
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-10 08:04:58 +0500

    feat: add shopSearchProvider provider

--- lib/pages/waiting_shops/parts/search_wainting_shops.dart
+++ lib/pages/waiting_shops/parts/search_wainting_shops.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_super_admin/pages/parts/search_input.dart';
+import 'package:skar_super_admin/providers/pages/shops.dart';
 
-class SearchWaintingShops extends StatelessWidget {
+class SearchWaintingShops extends ConsumerWidget {
   const SearchWaintingShops({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     var lang = AppLocalizations.of(context)!;
 
     return SearchInput(
       label: lang.searchShop,
       onSubmitted: (value) {
-        // ref.read(productSearchProvider.notifier).state = value;
-        // ref.read(hasProductsProvider.notifier).state = true;
+        ref.read(shopSearchProvider.notifier).state = value;
+        ref.read(hasShopsProvider.notifier).state = true;
       },
     );
   }
--- lib/providers/pages/shops.dart
+++ lib/providers/pages/shops.dart
@@ -2,3 +2,4 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var hasShopsProvider = StateProvider.autoDispose<bool>((ref) => true);
 var hasDeletedShopsProvider = StateProvider.autoDispose<bool>((ref) => true);
+var shopSearchProvider = StateProvider<String>((ref) => '');
