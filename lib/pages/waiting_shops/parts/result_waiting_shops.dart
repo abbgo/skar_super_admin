27cdee3285ea06f7365782e04ab2311cc159e508
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-11 11:04:04 +0500

    fix: update ResultWaitingShops

--- lib/pages/waiting_shops/parts/result_waiting_shops.dart
+++ lib/pages/waiting_shops/parts/result_waiting_shops.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:skar_super_admin/pages/parts/shops_table/parts/shops_table_row.dart';
+import 'package:skar_super_admin/helpers/static_data.dart';
+import 'package:skar_super_admin/models/shop.dart';
+import 'package:skar_super_admin/providers/api/shop.dart';
+import 'package:skar_super_admin/services/api/shop.dart';
 
 class ResultWaitingShops extends ConsumerWidget {
   const ResultWaitingShops({super.key});
@@ -9,17 +12,68 @@ class ResultWaitingShops extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 10),
-      child: Table(
-        border: TableBorder.all(),
-        children: const [
-          TableRow(
+      child: Column(
+        children: [
+          Row(
             children: [
-              ShopsTableRow(text: 'Ady (tm)', isHead: true),
-              ShopsTableRow(text: 'Ady (ru)', isHead: true),
-              ShopsTableRow(text: 'Eltip bermek hyzmaty barmy', isHead: true),
-              ShopsTableRow(text: 'Surat', isHead: true),
-              ShopsTableRow(text: 'Surat', isHead: true),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(5),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: const Text('Ady (tm)'),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: const Text('Ady (ru)'),
+                ),
+              ),
             ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                final page = index ~/ pageSize + 1;
+                final indexInPage = index % pageSize;
+
+                ShopParams shopParams = ShopParams(
+                  page: page,
+                  isDeleted: false,
+                  context: context,
+                  // cratedStatuses: const ['${CreatedStatuses.wait}'],
+                  cratedStatuses: const [],
+                );
+                final AsyncValue<ResultShop> shops =
+                    ref.watch(fetchShopsProvider(shopParams));
+
+                return shops.when(
+                  skipLoadingOnRefresh: true,
+                  skipLoadingOnReload: true,
+                  skipError: true,
+                  data: (response) {
+                    if (response.error != '' || response.shops == null) {
+                      return null;
+                    }
+                    if (indexInPage >= response.shops!.length) {
+                      return null;
+                    }
+                    Shop shop = response.shops![indexInPage];
+                    return Text(shop.nameTM);
+                  },
+                  error: (error, stackTrace) => errorMethod(error),
+                  loading: () => null,
+                );
+              },
+            ),
           ),
         ],
       ),
--- lib/pages/waiting_shops/waiting_shops.dart
+++ lib/pages/waiting_shops/waiting_shops.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:skar_super_admin/helpers/static_data.dart';
-import 'package:skar_super_admin/models/shop.dart';
 import 'package:skar_super_admin/pages/parts/drawer/drawer.dart';
 import 'package:skar_super_admin/pages/parts/my_app_bar/my_app_bar.dart';
 import 'package:skar_super_admin/pages/parts/no_result.dart';
-import 'package:skar_super_admin/pages/parts/shops_table/shops_table.dart';
 import 'package:skar_super_admin/pages/waiting_shops/parts/result_waiting_shops.dart';
-import 'package:skar_super_admin/providers/api/shop.dart';
 import 'package:skar_super_admin/providers/pages/shops.dart';
-import 'package:skar_super_admin/services/api/shop.dart';
 
 class WaitingShops extends ConsumerWidget {
   const WaitingShops({super.key});
@@ -19,44 +14,10 @@ class WaitingShops extends ConsumerWidget {
     bool hasShops = ref.watch(hasShopsProvider);
 
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: myAppBar('waiting_shops'),
       drawer: const DrawerPart(),
       body: !hasShops ? const NoResult() : const ResultWaitingShops(),
-      // : ListView.builder(
-      //     physics: const BouncingScrollPhysics(),
-      //     itemBuilder: (context, index) {
-      //       final page = index ~/ pageSize + 1;
-      //       final indexInPage = index % pageSize;
-
-      //       ShopParams shopParams = ShopParams(
-      //         page: page,
-      //         isDeleted: false,
-      //         context: context,
-      //         cratedStatuses: const ['${CreatedStatuses.wait}'],
-      //       );
-      //       final AsyncValue<ResultShop> shops =
-      //           ref.watch(fetchShopsProvider(shopParams));
-
-      //       return shops.when(
-      //         skipLoadingOnRefresh: true,
-      //         skipLoadingOnReload: true,
-      //         skipError: true,
-      //         data: (response) {
-      //           if (response.error != '' || response.shops == null) {
-      //             return null;
-      //           }
-      //           if (indexInPage >= response.shops!.length) {
-      //             return null;
-      //           }
-      //           Shop shop = response.shops![indexInPage];
-      //           // return Text(shop.nameTM);
-      //           return ShopsTable();
-      //         },
-      //         error: (error, stackTrace) => errorMethod(error),
-      //         loading: () => null,
-      //       );
-      //     },
-      //   ),
     );
   }
 }
