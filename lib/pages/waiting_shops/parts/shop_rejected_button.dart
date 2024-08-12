4039e9cd51e102fa28c267e3fd6bda92033cba88
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-12 10:28:16 +0500

    fix: update ShopRejectedButton part widget

--- lib/pages/waiting_shops/parts/shop_rejected_button.dart
+++ lib/pages/waiting_shops/parts/shop_rejected_button.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_super_admin/helpers/methods/toasts.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:skar_super_admin/models/shop_created_status.dart';
+import 'package:skar_super_admin/providers/api/shop.dart';
+import 'package:skar_super_admin/services/api/shop.dart';
 
 class ShopRejectedButton extends ConsumerWidget {
   const ShopRejectedButton({super.key, required this.shopID});
@@ -14,7 +17,13 @@ class ShopRejectedButton extends ConsumerWidget {
 
     return IconButton(
       onPressed: () async {
+        ShopParams params = ShopParams(
+          context: context,
+          shopCreatedStatus: ShopCreatedStatus(id: shopID, createdStatus: 1),
+        );
+        await ref.watch(updateShopCreatedStatusProvider(params).future);
         showToast(lang.shopRejected, false);
+        ref.invalidate(fetchShopsProvider);
       },
       icon: const Icon(Icons.cancel, color: Colors.red),
     );
