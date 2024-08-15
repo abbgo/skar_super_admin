ea4b2fb3e632fabd48747de622507b163c9ebb4b
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-15 12:11:06 +0500

    feat: add ResultProducts part widget

---
+++ lib/pages/waiting_products/parts/result_products.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ResultProducts extends StatelessWidget {
+  const ResultProducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Result Products'));
+  }
+}
--- lib/pages/waiting_products/waiting_products.dart
+++ lib/pages/waiting_products/waiting_products.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:skar_super_admin/pages/parts/drawer/drawer.dart';
 import 'package:skar_super_admin/pages/parts/my_app_bar/my_app_bar.dart';
+import 'package:skar_super_admin/pages/waiting_products/parts/result_products.dart';
 
 class WaitingProductsPage extends StatelessWidget {
   const WaitingProductsPage({super.key});
@@ -11,7 +12,7 @@ class WaitingProductsPage extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       appBar: myAppBar('waiting_products'),
       drawer: const DrawerPart(),
-      body: const Center(child: Text('Waiting Products')),
+      body: const ResultProducts(),
     );
   }
 }
