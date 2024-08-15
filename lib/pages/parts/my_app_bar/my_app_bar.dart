89f18f08cd7b1e123d1c4954f7f7462606728d62
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-15 12:08:53 +0500

    fix: update myAppBar methpd

--- lib/pages/parts/my_app_bar/my_app_bar.dart
+++ lib/pages/parts/my_app_bar/my_app_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:skar_super_admin/pages/parts/my_app_bar/parts/avatar.dart';
 import 'package:skar_super_admin/pages/parts/my_app_bar/parts/log_out_button.dart';
 import 'package:skar_super_admin/pages/parts/my_app_bar/parts/pages_app_bar.dart';
+import 'package:skar_super_admin/providers/pages/products.dart';
 import 'package:skar_super_admin/providers/pages/shops.dart';
 
 AppBar myAppBar(String page) {
@@ -15,10 +16,10 @@ AppBar myAppBar(String page) {
       titlePage = PagesAppBar(searchProvider: shopSearchProvider);
       break;
     case 'waiting_products':
-      titlePage = const PagesAppBar();
+      titlePage = PagesAppBar(searchProvider: productSearchProvider);
       break;
     case 'active_products':
-      titlePage = const PagesAppBar();
+      titlePage = PagesAppBar(searchProvider: productSearchProvider);
       break;
     default:
   }
