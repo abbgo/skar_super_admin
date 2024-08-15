6f8f6ed25417f9ddd3157f92987c5562d3f3318e
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-15 06:42:25 +0500

    feat: add RejectedShopCommentDialog part widget

--- lib/helpers/methods/dialogs.dart
+++ lib/helpers/methods/dialogs.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:photo_view/photo_view.dart';
-import 'package:skar_super_admin/helpers/methods/toasts.dart';
 import 'package:skar_super_admin/helpers/static_data.dart';
-import 'package:skar_super_admin/models/shop_created_status.dart';
-import 'package:skar_super_admin/pages/parts/rejected_shop_comment_dialog_content.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:skar_super_admin/providers/api/shop.dart';
-import 'package:skar_super_admin/services/api/shop.dart';
+import 'package:skar_super_admin/pages/parts/rejected_shop_comment_dialog/rejected_shop_comment_dialog.dart';
 
 showImageDialog(BuildContext context, String image) => showDialog(
       context: context,
@@ -25,43 +19,5 @@ showImageDialog(BuildContext context, String image) => showDialog(
 showRejectedShopCommentDialog(BuildContext context, String shopID) =>
     showDialog(
       context: context,
-      builder: (context) {
-        var lang = AppLocalizations.of(context)!;
-        final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
-        return AlertDialog.adaptive(
-          content: RejectedShopCommentDialogContent(formKey: formKey),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text(
-                lang.cancel,
-                style: const TextStyle(color: Colors.red),
-              ),
-            ),
-            Consumer(
-              builder: (context, ref, child) => TextButton(
-                onPressed: () async {
-                  if (formKey.currentState?.validate() == true) {
-                    ShopParams params = ShopParams(
-                      context: context,
-                      shopCreatedStatus:
-                          ShopCreatedStatus(id: shopID, createdStatus: 1),
-                    );
-                    await ref
-                        .watch(updateShopCreatedStatusProvider(params).future);
-                    showToast(lang.shopRejected, false);
-                    ref.invalidate(fetchShopsProvider);
-                    if (context.mounted) Navigator.pop(context);
-                  }
-                },
-                child: Text(
-                  lang.rejected,
-                  style: const TextStyle(color: Colors.green),
-                ),
-              ),
-            ),
-          ],
-        );
-      },
+      builder: (context) => RejectedShopCommentDialog(shopID: shopID),
     );
---
+++ lib/pages/parts/rejected_shop_comment_dialog/rejected_shop_comment_dialog.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_super_admin/helpers/methods/toasts.dart';
+import 'package:skar_super_admin/models/shop_created_status.dart';
+import 'package:skar_super_admin/pages/parts/rejected_shop_comment_dialog/rejected_shop_comment_dialog_content.dart';
+import 'package:skar_super_admin/providers/api/shop.dart';
+import 'package:skar_super_admin/services/api/shop.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class RejectedShopCommentDialog extends StatefulWidget {
+  const RejectedShopCommentDialog({super.key, required this.shopID});
+
+  final String shopID;
+
+  @override
+  State<RejectedShopCommentDialog> createState() =>
+      _RejectedShopCommentDialogState();
+}
+
+class _RejectedShopCommentDialogState extends State<RejectedShopCommentDialog> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    var lang = AppLocalizations.of(context)!;
+
+    return AlertDialog.adaptive(
+      content: RejectedShopCommentDialogContent(formKey: formKey),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: Text(
+            lang.cancel,
+            style: const TextStyle(color: Colors.red),
+          ),
+        ),
+        Consumer(
+          builder: (context, ref, child) => TextButton(
+            onPressed: () async {
+              if (formKey.currentState?.validate() == true) {
+                ShopParams params = ShopParams(
+                  context: context,
+                  shopCreatedStatus:
+                      ShopCreatedStatus(id: widget.shopID, createdStatus: 1),
+                );
+                await ref.watch(updateShopCreatedStatusProvider(params).future);
+                showToast(lang.shopRejected, false);
+                ref.invalidate(fetchShopsProvider);
+                if (context.mounted) Navigator.pop(context);
+              }
+            },
+            child: Text(
+              lang.rejected,
+              style: const TextStyle(color: Colors.green),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
