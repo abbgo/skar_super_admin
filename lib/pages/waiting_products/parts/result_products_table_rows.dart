2018ae7ec7468c50367109f33f5211a9964725eb
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-16 12:48:27 +0500

    fix: update ResultProductsTableRows part widget

--- lib/pages/waiting_products/parts/result_products_table_rows.dart
+++ lib/pages/waiting_products/parts/result_products_table_rows.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_super_admin/models/product.dart';
 import 'package:skar_super_admin/pages/waiting_shops/parts/result_table_header.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:skar_super_admin/providers/local_storadge.dart';
 
 class ResultProductsTableRows extends StatelessWidget {
   const ResultProductsTableRows({super.key, required this.product});
@@ -25,7 +27,15 @@ class ResultProductsTableRows extends StatelessWidget {
           text: product.brend != null ? product.brend!.name : '-',
           isHeader: false,
         ),
-        ResultTableHeader(text: product.shop!.nameTM!, isHeader: false),
+        Consumer(
+          builder: (context, ref, child) {
+            bool isTM = ref.read(langProvider) == 'tr';
+            return ResultTableHeader(
+              text: isTM ? product.shop!.nameTM! : product.shop!.nameRU!,
+              isHeader: false,
+            );
+          },
+        ),
         ResultTableHeader(
           text: product.isVisible ? lang.yes : lang.no,
           isHeader: false,
