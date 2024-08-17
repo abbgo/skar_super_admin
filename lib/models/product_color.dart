6f44a0c4ea546cc00f624cdc90a244517af0628d
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-17 11:12:48 +0500

    fix: update ProductColor model

--- lib/models/product_color.dart
+++ lib/models/product_color.dart
@@ -1,83 +1,24 @@
-import 'package:equatable/equatable.dart';
-import 'package:skar_super_admin/models/dimension.dart';
-import 'package:skar_super_admin/models/image.dart';
-
-class ProductColor extends Equatable {
+class ProductColor {
   final String name;
-  final int orderNumber;
-  final List<dynamic>? dimensions;
-  final List<ProductColorImage> images;
-  final List<Dimension>? selectedDimensions;
+  final List<String> dimensions;
+  final List<String> images;
 
-  const ProductColor({
-    required this.name,
-    required this.orderNumber,
-    this.dimensions,
-    required this.images,
-    this.selectedDimensions,
-  });
+  const ProductColor(
+      {required this.name, required this.dimensions, required this.images});
 
   factory ProductColor.defaultProductColor() {
-    return const ProductColor(
-      name: '',
-      orderNumber: 0,
-      dimensions: [],
-      images: [],
-      selectedDimensions: [],
-    );
+    return const ProductColor(name: '', dimensions: [], images: []);
   }
 
   factory ProductColor.fromJson(Map<String, dynamic> json) {
     return ProductColor(
       name: json['name'],
-      orderNumber: json['order_number'],
-      selectedDimensions: json['dimensions'] == null
-          ? []
-          : List<Dimension>.from(
-              json['dimensions'].map(
-                (dimensionJson) => Dimension.fromJson(dimensionJson),
-              ),
-            ),
-      images: List<ProductColorImage>.from(
-        json['images'].map(
-          (productColorImageJson) =>
-              ProductColorImage.fromJson(productColorImageJson),
-        ),
-      ),
+      dimensions: json['dimensions'],
+      images: json['images'],
     );
   }
 
   Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-      'order_number': orderNumber,
-      'images': images,
-      'dimensions': dimensions,
-    };
+    return {'name': name, 'images': images, 'dimensions': dimensions};
   }
-
-  ProductColor copyWith({
-    String? name,
-    int? orderNumber,
-    List<dynamic>? dimensions,
-    List<ProductColorImage>? images,
-    final List<Dimension>? selectedDimensions,
-  }) {
-    return ProductColor(
-      name: name ?? this.name,
-      orderNumber: orderNumber ?? this.orderNumber,
-      dimensions: dimensions ?? this.dimensions,
-      images: images ?? this.images,
-      selectedDimensions: selectedDimensions ?? this.selectedDimensions,
-    );
-  }
-
-  @override
-  List<Object?> get props => [
-        name,
-        orderNumber,
-        dimensions,
-        images,
-        selectedDimensions,
-      ];
 }
