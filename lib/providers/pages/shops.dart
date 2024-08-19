b159f41f70f39b1825c4ba3983d96c5e82ee69a1
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-19 10:36:09 +0500

    feat: add loadShopsProvider provider

--- lib/providers/pages/shops.dart
+++ lib/providers/pages/shops.dart
@@ -1,3 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var shopSearchProvider = StateProvider<String>((ref) => '');
+var loadShopsProvider = StateProvider.autoDispose<bool>((ref) => true);
