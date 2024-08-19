017b970769d28de66aa213dd398451cc5afaa5bb
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-19 11:45:58 +0500

    fix: update ResultProducts part widget

--- lib/pages/waiting_products/parts/result_products.dart
+++ lib/pages/waiting_products/parts/result_products.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_super_admin/helpers/functions/screen.dart';
 import 'package:skar_super_admin/helpers/static_data.dart';
 import 'package:skar_super_admin/models/product.dart';
 import 'package:skar_super_admin/pages/waiting_products/parts/result_products_table_headers.dart';
 import 'package:skar_super_admin/pages/waiting_products/parts/result_products_table_rows.dart';
 import 'package:skar_super_admin/providers/api/product.dart';
+import 'package:skar_super_admin/providers/pages/products.dart';
 import 'package:skar_super_admin/services/api/product.dart';
 
 class ResultProducts extends ConsumerWidget {
@@ -14,45 +16,61 @@ class ResultProducts extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    bool load = cratedStatus == CreatedStatuses.wait
+        ? ref.watch(loadWaitingProductsProvider)
+        : ref.watch(loadActiveProductsProvider);
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 10),
       child: Column(
         children: [
           const ResultProductsTableHeaders(),
           Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                final page = index ~/ pageSize + 1;
-                final indexInPage = index % pageSize;
+            child: Stack(
+              children: [
+                ListView.builder(
+                  itemBuilder: (context, index) {
+                    final page = index ~/ pageSize + 1;
+                    final indexInPage = index % pageSize;
 
-                ProductParams params = ProductParams(
-                  page: page,
-                  isDeleted: false,
-                  context: context,
-                  cratedStatuses: ['$cratedStatus'],
-                );
+                    ProductParams params = ProductParams(
+                      page: page,
+                      isDeleted: false,
+                      context: context,
+                      cratedStatuses: ['$cratedStatus'],
+                    );
 
-                AsyncValue<ResultProduct> resultProduct =
-                    ref.watch(fetchProductsProvider(params));
+                    AsyncValue<ResultProduct> resultProduct =
+                        ref.watch(fetchProductsProvider(params));
 
-                return resultProduct.when(
-                  skipLoadingOnRefresh: true,
-                  skipLoadingOnReload: true,
-                  skipError: true,
-                  data: (response) {
-                    if (response.error != '' || response.products == null) {
-                      return null;
-                    }
-                    if (indexInPage >= response.products!.length) {
-                      return null;
-                    }
-                    Product product = response.products![indexInPage];
-                    return ResultProductsTableRows(product: product);
+                    return resultProduct.when(
+                      skipLoadingOnRefresh: true,
+                      skipLoadingOnReload: true,
+                      skipError: true,
+                      data: (response) {
+                        if (response.error != '' || response.products == null) {
+                          return null;
+                        }
+                        if (indexInPage >= response.products!.length) {
+                          return null;
+                        }
+                        Product product = response.products![indexInPage];
+                        return ResultProductsTableRows(product: product);
+                      },
+                      error: (error, stackTrace) => errorMethod(error),
+                      loading: () => null,
+                    );
                   },
-                  error: (error, stackTrace) => errorMethod(error),
-                  loading: () => null,
-                );
-              },
+                ),
+                load
+                    ? Container(
+                        height: screenProperties(context).height,
+                        width: screenProperties(context).width,
+                        color: Colors.white.withOpacity(.2),
+                        child: loadWidget,
+                      )
+                    : const SizedBox(),
+              ],
             ),
           ),
         ],
