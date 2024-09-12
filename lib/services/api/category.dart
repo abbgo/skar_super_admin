022c404718da74e5d97d0b9dd439f3b74860b143
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-12 12:28:35 +0500

    feat: add fetchCategories function

--- lib/services/api/category.dart
+++ lib/services/api/category.dart
@@ -1,1 +1,45 @@
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+import 'package:skar_super_admin/helpers/static_data.dart';
+import 'package:skar_super_admin/models/category.dart';
+
+class CategoryApiService {
+  // fetch categroies --------------------------------------------
+  Future<ResultCategory> fetchCategories(
+    String search,
+    int page,
+    String lang,
+  ) async {
+    Uri uri = Uri.parse('$apiUrl/categories').replace(
+      queryParameters: {
+        'limit': pageSize.toString(),
+        'page': '$page',
+        'search': search,
+        'lang': lang
+      },
+    );
+
+    try {
+      http.Response response = await http.get(uri);
+      var jsonData = json.decode(response.body);
+
+      if (response.statusCode == 200 && jsonData['status']) {
+        if (jsonData['categories'] == null) {
+          return const ResultCategory(categories: [], error: '');
+        }
+
+        var categoriesList = jsonData['categories'] as List;
+        return ResultCategory(
+          categories: categoriesList
+              .map<Category>((propJson) => Category.fromJson(propJson))
+              .toList(),
+          error: '',
+        );
+      }
+      return const ResultCategory(categories: [], error: '');
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
