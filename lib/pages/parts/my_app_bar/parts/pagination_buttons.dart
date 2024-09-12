7c1ee929c1fbbc4b15273e197a6714e85963e3eb
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-12 10:57:08 +0500

    fix: update PaginationButtons part widget

--- lib/pages/parts/my_app_bar/parts/pagination_buttons.dart
+++ lib/pages/parts/my_app_bar/parts/pagination_buttons.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_super_admin/providers/pages/shops.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class PaginationButtons extends ConsumerWidget {
   const PaginationButtons({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var lang = AppLocalizations.of(context)!;
     int shopPage = ref.watch(shopPageProvider);
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Text('Sahypa: $shopPage'),
+        Text('${lang.page}: $shopPage'),
         IconButton(
           onPressed: () =>
               ref.read(shopPageProvider.notifier).state = shopPage - 1,
