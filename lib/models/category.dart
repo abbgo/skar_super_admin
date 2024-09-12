80f48e22d7bfae8c9a158755e31efb7b7201a4b9
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-12 12:26:22 +0500

    feat: add parentCategoryID and childCategories columns to Category model

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,10 +1,24 @@
 class Category {
   final String id, nameTM, nameRU;
+  final String? parentCategoryID;
+  final List<Category>? childCategories;
 
-  Category({required this.id, required this.nameTM, required this.nameRU});
+  Category({
+    required this.id,
+    required this.nameTM,
+    required this.nameRU,
+    this.parentCategoryID,
+    this.childCategories,
+  });
 
   factory Category.defaultCategory() {
-    return Category(id: '', nameTM: '', nameRU: '');
+    return Category(
+      id: '',
+      nameTM: '',
+      nameRU: '',
+      parentCategoryID: null,
+      childCategories: null,
+    );
   }
 
   factory Category.fromJson(Map<String, dynamic> json) {
@@ -12,6 +26,14 @@ class Category {
       id: json['id'],
       nameTM: json['name_tm'],
       nameRU: json['name_ru'],
+      parentCategoryID: json['parent_category_id'] ?? '',
+      childCategories: json['child_categories'] == null
+          ? []
+          : List<Category>.from(
+              json['child_categories'].map(
+                (categoryJson) => Category.fromJson(categoryJson),
+              ),
+            ),
     );
   }
 }
