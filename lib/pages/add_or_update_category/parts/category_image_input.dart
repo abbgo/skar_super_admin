84a3753f963b556cf5a7c56f430839a52e2d5283
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-13 17:28:38 +0500

    fix: update CategoryImageInput part widget

--- lib/pages/add_or_update_category/parts/category_image_input.dart
+++ lib/pages/add_or_update_category/parts/category_image_input.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class CategoryImageInput extends StatelessWidget {
   const CategoryImageInput({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Expanded(
-      child: Row(
-        children: [Text('Surat saylan')],
+    var lang = AppLocalizations.of(context)!;
+
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Text(lang.addImageOfTheCategory),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    // FocusManager.instance.primaryFocus!.unfocus();
+                    // showSelectedImageSheet(context, 'shop', 4, 3);
+                  },
+                  icon: const Icon(Icons.add_photo_alternate, size: 100),
+                ),
+                const Expanded(
+                  child: Text('surat gorunmeli'),
+                  // child: !loadSendImage
+                  //     ? selectedImage.isEmpty
+                  //         ? Text(lang.noImage, textAlign: TextAlign.center)
+                  //         : AspectRatio(
+                  //             aspectRatio: 4 / 3,
+                  //             child: showCachImageMethod(selectedImage),
+                  //           )
+                  //     : loadWidget,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
