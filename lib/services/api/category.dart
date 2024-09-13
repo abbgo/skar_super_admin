18dd421dc726fba14fa7d1177f8369feb0f8e0b4
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-13 16:17:57 +0500

    feat: add fetchCategoriesWithChild function

--- lib/services/api/category.dart
+++ lib/services/api/category.dart
@@ -5,7 +5,7 @@ import 'package:skar_super_admin/helpers/static_data.dart';
 import 'package:skar_super_admin/models/category.dart';
 
 class CategoryApiService {
-  // fetch categroies --------------------------------------------
+  // fetch categroies with child --------------------------------------------
   Future<ResultCategory> fetchCategoriesWithChild(
     String accessToken,
     String search,
@@ -13,6 +13,53 @@ class CategoryApiService {
     String lang,
   ) async {
     Uri uri = Uri.parse('$apiUrl/back/categories/with-child').replace(
+      queryParameters: {
+        'limit': '100',
+        'page': '$page',
+        'search': search,
+        'lang': lang
+      },
+    );
+
+    try {
+      http.Response response = await http.get(
+        uri,
+        headers: tokenHeader(accessToken),
+      );
+      var jsonData = json.decode(response.body);
+
+      if (response.statusCode == 200 && jsonData['status']) {
+        if (jsonData['categories'] == null) {
+          return const ResultCategory(
+            categories: null,
+            pageCount: 0,
+            error: '',
+          );
+        }
+
+        var categoriesList = jsonData['categories'] as List;
+        return ResultCategory(
+          categories: categoriesList
+              .map<Category>((propJson) => Category.fromJson(propJson))
+              .toList(),
+          pageCount: jsonData['page_count'],
+          error: '',
+        );
+      }
+      return const ResultCategory(categories: null, pageCount: 0, error: '');
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+// fetch categroies --------------------------------------------
+  Future<ResultCategory> fetchCategories(
+    String accessToken,
+    String search,
+    int page,
+    String lang,
+  ) async {
+    Uri uri = Uri.parse('$apiUrl/back/categories').replace(
       queryParameters: {
         'limit': '100',
         'page': '$page',
