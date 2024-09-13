bda9b7b08f38139b8307d927976e02767d0d9ccf
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-13 14:53:46 +0500

    feat: add EditCategoryButton part widget

--- lib/pages/categories/parts/categories_table_buttons.dart
+++ lib/pages/categories/parts/categories_table_buttons.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:skar_super_admin/models/category.dart';
 import 'package:skar_super_admin/pages/categories/parts/child_categories_button.dart';
+import 'package:skar_super_admin/pages/categories/parts/edit_category_button.dart';
 
 class CategoriesTableButtons extends StatelessWidget {
   const CategoriesTableButtons({super.key, this.subcategories});
@@ -12,6 +13,8 @@ class CategoriesTableButtons extends StatelessWidget {
     return Column(
       children: [
         ChildCategoriesButton(subcategories: subcategories),
+        const SizedBox(height: 10),
+        const EditCategoryButton(),
       ],
     );
   }
---
+++ lib/pages/categories/parts/edit_category_button.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class EditCategoryButton extends StatelessWidget {
+  const EditCategoryButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var lang = AppLocalizations.of(context)!;
+
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
+      onPressed: () {},
+      child: Text(lang.edit),
+    );
+  }
+}
