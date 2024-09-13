78f6c087c05929c4175f312e3f259f1e09dc0f79
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-13 15:25:37 +0500

    feat: add AddOrUpdateCategoryBody part widget

--- lib/pages/add_or_update_category/add_or_update_category.dart
+++ lib/pages/add_or_update_category/add_or_update_category.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skar_super_admin/pages/add_or_update_category/add_or_update_category_body.dart';
 
 class AddOrUpdateCategoryPage extends StatelessWidget {
   const AddOrUpdateCategoryPage({super.key});
@@ -12,9 +13,11 @@ class AddOrUpdateCategoryPage extends StatelessWidget {
           icon: const Icon(Icons.arrow_back),
         ),
       ),
-      body: const Center(
-        child: Text('Add or update category'),
-      ),
+      body: addOrUpdateCategoryBody(),
     );
+  }
+
+  AddOrUpdateCategoryBody addOrUpdateCategoryBody() {
+    return AddOrUpdateCategoryBody();
   }
 }
---
+++ lib/pages/add_or_update_category/add_or_update_category_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+
+class AddOrUpdateCategoryBody extends StatelessWidget {
+  const AddOrUpdateCategoryBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Form(
+          child: ListView(),
+        ),
+      ],
+    );
+  }
+}
