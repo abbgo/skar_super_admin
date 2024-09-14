411d6d2764a38e58938163db36c7b5b844e624dd
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-14 15:02:17 +0500

    feat: add loadCreateCategoryProvider provider

--- lib/pages/add_or_update_category/parts/add_or_update_category_button.dart
+++ lib/pages/add_or_update_category/parts/add_or_update_category_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_super_admin/providers/parts/file_upload.dart';
+import 'package:skar_super_admin/services/api/category.dart';
 
 class AddOrUpdateCategoryButton extends ConsumerWidget {
   const AddOrUpdateCategoryButton({
@@ -25,7 +26,9 @@ class AddOrUpdateCategoryButton extends ConsumerWidget {
           onPressed: () async {
             String imagePath = await ref.read(imagePathProvider);
             if (formKey.currentState?.validate() == true &&
-                imagePath.isNotEmpty) {}
+                imagePath.isNotEmpty) {
+              CategoryParams? arg;
+            }
           },
           child: const Text('Gos'),
         ),
--- lib/providers/pages/add_or_update_category.dart
+++ lib/providers/pages/add_or_update_category.dart
@@ -1,3 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final parentCategoryProvider = StateProvider<String>((ref) => '');
+final loadCreateCategoryProvider =
+    StateProvider.autoDispose<bool>((ref) => false);
