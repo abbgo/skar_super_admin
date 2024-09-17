a1b12b13deaafcb66e10a59b437bdc593be0a4e8
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-17 16:18:43 +0500

    feat: add ShoppingCentersButton part widget

--- lib/pages/parts/drawer/drawer.dart
+++ lib/pages/parts/drawer/drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:skar_super_admin/pages/parts/drawer/parts/categories_button.dart';
 import 'package:skar_super_admin/pages/parts/drawer/parts/home_button.dart';
 import 'package:skar_super_admin/pages/parts/drawer/parts/products_button.dart';
+import 'package:skar_super_admin/pages/parts/drawer/parts/shopping_centers_button.dart';
 import 'package:skar_super_admin/pages/parts/drawer/parts/shops_button.dart';
 
 class DrawerPart extends StatelessWidget {
@@ -18,6 +19,7 @@ class DrawerPart extends StatelessWidget {
           const ShopsButton(),
           const ProductsButton(),
           const CategoriesButton(),
+          const ShoppingCentersButton(),
         ],
       ),
     );
---
+++ lib/pages/parts/drawer/parts/shopping_centers_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_super_admin/helpers/methods/navigators.dart';
+import 'package:skar_super_admin/pages/categories/categories.dart';
+import 'package:skar_super_admin/providers/pages/drawer.dart';
+import 'package:skar_super_admin/styles/colors.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class ShoppingCentersButton extends ConsumerWidget {
+  const ShoppingCentersButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var lang = AppLocalizations.of(context)!;
+    int selectedDrawerButton = ref.watch(selectedDrawerButtonProvider);
+
+    return ListTile(
+      leading: Icon(Icons.view_cozy_outlined, color: logoColor),
+      tileColor: selectedDrawerButton == 6 ? elevatedButtonColor : null,
+      title: Text(
+        lang.malls,
+        style: TextStyle(
+          color: selectedDrawerButton == 6 ? Colors.white : elevatedButtonColor,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: () {
+        ref.read(selectedDrawerButtonProvider.notifier).state = 6;
+        goToPage(context, const CategoriesPage(), true);
+      },
+    );
+  }
+}
