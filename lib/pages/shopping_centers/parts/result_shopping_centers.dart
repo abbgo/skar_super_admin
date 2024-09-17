8548fbb0c493ac6aca735302bee6ba900528b59b
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-17 17:40:20 +0500

    fix: update ResultShoppingCenters part widget

--- lib/pages/shopping_centers/parts/result_shopping_centers.dart
+++ lib/pages/shopping_centers/parts/result_shopping_centers.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_super_admin/helpers/static_data.dart';
+import 'package:skar_super_admin/models/shop.dart';
+import 'package:skar_super_admin/providers/api/shop.dart';
+import 'package:skar_super_admin/services/api/shop.dart';
+import 'package:skar_super_admin/styles/colors.dart';
 
-class ResultShoppingCenters extends StatelessWidget {
+class ResultShoppingCenters extends ConsumerWidget {
   const ResultShoppingCenters({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Shopping Centers'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    ShopParams arg = ShopParams(
+      isDeleted: false,
+      context: context,
+    );
+    final AsyncValue<ResultShop> resultShop =
+        ref.watch(fetchShoppingCentersProvider(arg));
+
+    return resultShop.when(
+      data: (response) {
+        if (response.error != '' || response.shops == null) {
+          return const SizedBox();
+        }
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 10),
+          child: Column(
+            children: [
+              Table(
+                border: TableBorder.all(),
+                children: [
+                  TableRow(
+                    children: shopColumns(context),
+                    decoration: BoxDecoration(color: elevatedButtonColor),
+                  ),
+                ],
+              ),
+              // Expanded(
+              //   child: SingleChildScrollView(
+              //     child: Table(
+              //       border: TableBorder.all(),
+              //       children: shopRows(response.shops!, context, cratedStatus),
+              //     ),
+              //   ),
+              // ),
+            ],
+          ),
+        );
+      },
+      error: (error, stackTrace) => errorMethod(error),
+      loading: () => loadWidget,
     );
   }
 }
