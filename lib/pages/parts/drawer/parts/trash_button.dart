713b19988aebe9336e8b012707451ca8dc4fe406
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-17 18:36:12 +0500

    feat: add Trashpage

--- lib/pages/parts/drawer/parts/trash_button.dart
+++ lib/pages/parts/drawer/parts/trash_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_super_admin/helpers/methods/navigators.dart';
-import 'package:skar_super_admin/pages/shopping_centers/shopping_centers.dart';
+import 'package:skar_super_admin/pages/trash/trash.dart';
 import 'package:skar_super_admin/providers/pages/drawer.dart';
 import 'package:skar_super_admin/styles/colors.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -16,17 +16,17 @@ class TrashButton extends ConsumerWidget {
 
     return ListTile(
       leading: Icon(Icons.auto_delete, color: logoColor),
-      tileColor: selectedDrawerButton == 6 ? elevatedButtonColor : null,
+      tileColor: selectedDrawerButton == 7 ? elevatedButtonColor : null,
       title: Text(
         lang.trash,
         style: TextStyle(
-          color: selectedDrawerButton == 6 ? Colors.white : elevatedButtonColor,
+          color: selectedDrawerButton == 7 ? Colors.white : elevatedButtonColor,
           fontWeight: FontWeight.bold,
         ),
       ),
       onTap: () {
-        ref.read(selectedDrawerButtonProvider.notifier).state = 6;
-        goToPage(context, const ShoppingCentersPage(), true);
+        ref.read(selectedDrawerButtonProvider.notifier).state = 7;
+        goToPage(context, const Trashpage(), true);
       },
     );
   }
---
+++ lib/pages/trash/trash.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:skar_super_admin/pages/parts/drawer/drawer.dart';
+import 'package:skar_super_admin/pages/parts/my_app_bar/my_app_bar.dart';
+
+class Trashpage extends StatelessWidget {
+  const Trashpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: myAppBar('trash'),
+      drawer: const DrawerPart(),
+      body: const Center(
+        child: Text('Trash'),
+      ),
+    );
+  }
+}
