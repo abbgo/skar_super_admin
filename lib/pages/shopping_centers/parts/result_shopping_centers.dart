674c3306309cf78acd684e99f4043ac03965a9da
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-17 17:36:40 +0500

    feat: add ResultShoppingCenters part widget

---
+++ lib/pages/shopping_centers/parts/result_shopping_centers.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ResultShoppingCenters extends StatelessWidget {
+  const ResultShoppingCenters({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Shopping Centers'),
+    );
+  }
+}
--- lib/pages/shopping_centers/shopping_centers.dart
+++ lib/pages/shopping_centers/shopping_centers.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:skar_super_admin/pages/parts/drawer/drawer.dart';
 import 'package:skar_super_admin/pages/parts/my_app_bar/my_app_bar.dart';
+import 'package:skar_super_admin/pages/shopping_centers/parts/result_shopping_centers.dart';
 
 class ShoppingCentersPage extends StatelessWidget {
   const ShoppingCentersPage({super.key});
@@ -10,9 +11,7 @@ class ShoppingCentersPage extends StatelessWidget {
     return Scaffold(
       appBar: myAppBar('shopping_centers'),
       drawer: const DrawerPart(),
-      body: const Center(
-        child: Text('Shopping Centers'),
-      ),
+      body: const ResultShoppingCenters(),
     );
   }
 }
