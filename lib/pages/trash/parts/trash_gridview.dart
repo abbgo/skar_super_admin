72a1d96bac06d6b0a1cb4ae347bf2f4c7e8dfd5b
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-18 11:14:26 +0500

    fix: update TrashGridview part widget

--- lib/pages/trash/parts/trash_gridview.dart
+++ lib/pages/trash/parts/trash_gridview.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:skar_super_admin/helpers/methods/navigators.dart';
+import 'package:skar_super_admin/pages/categories/categories.dart';
+import 'package:skar_super_admin/pages/shopping_centers/shopping_centers.dart';
 import 'package:skar_super_admin/styles/colors.dart';
 
 class TrashGridview extends StatelessWidget {
@@ -10,6 +13,7 @@ class TrashGridview extends StatelessWidget {
     var lang = AppLocalizations.of(context)!;
     List<String> texts = [lang.categoriesInTrash, lang.mallsInTrash];
     List<IconData> icons = [Icons.storefront, Icons.add_business];
+    List<Widget> pages = const [CategoriesPage(), ShoppingCentersPage()];
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 10),
@@ -22,18 +26,21 @@ class TrashGridview extends StatelessWidget {
         ),
         itemCount: texts.length,
         itemBuilder: (context, index) {
-          return Card(
-            color: elevatedButtonColor,
-            elevation: 6,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(icons[index], color: Colors.white, size: 50),
-                Text(
-                  texts[index],
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
+          return GestureDetector(
+            onTap: () => goToPage(context, pages[index], false),
+            child: Card(
+              color: elevatedButtonColor,
+              elevation: 6,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(icons[index], color: Colors.white, size: 50),
+                  Text(
+                    texts[index],
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
             ),
           );
         },
