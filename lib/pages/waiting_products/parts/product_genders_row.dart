55a9d152e04b612446849f5049d8d3b2cdbe7e05
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-08 07:19:12 +0500

    fix: update ProductGendersRow part widget

--- lib/pages/waiting_products/parts/product_genders_row.dart
+++ lib/pages/waiting_products/parts/product_genders_row.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:skar_super_admin/helpers/static_data.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ProductGendersRow extends StatelessWidget {
   const ProductGendersRow({super.key, required this.genders});
@@ -7,6 +9,21 @@ class ProductGendersRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(genders.toString());
+    var lang = AppLocalizations.of(context)!;
+
+    List<dynamic> selectedGenders = [];
+    if (genders.contains(Genders.male)) {
+      selectedGenders.add(lang.male);
+    }
+    if (genders.contains(Genders.female)) {
+      selectedGenders.add(lang.female);
+    }
+    if (genders.contains(Genders.child)) {
+      selectedGenders.add(lang.child);
+    }
+
+    return Column(
+      children: selectedGenders.map((e) => Text(e.toString())).toList(),
+    );
   }
 }
