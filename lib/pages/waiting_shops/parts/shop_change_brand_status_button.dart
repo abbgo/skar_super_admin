38dee335fc8eff3cc61cc65aab0a97ac7a3de564
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-10 12:38:31 +0500

    feat: add ShopChangeBrandStatusButton part widget

---
+++ lib/pages/waiting_shops/parts/shop_change_brand_status_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class ShopChangeBrandStatusButton extends StatelessWidget {
+  const ShopChangeBrandStatusButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var lang = AppLocalizations.of(context)!;
+
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+      onPressed: () async {
+        // ShopParams params = ShopParams(
+        //   context: context,
+        //   shopCreatedStatus: ShopCreatedStatus(id: shopID, createdStatus: 2),
+        // );
+        // await ref.watch(updateShopCreatedStatusProvider(params).future);
+        // showToast(lang.shopConfirmed, false);
+        // ref.invalidate(fetchShopsProvider);
+      },
+      child: Text(
+        '${lang.isItAnOfficialStore} ?',
+        style: const TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
